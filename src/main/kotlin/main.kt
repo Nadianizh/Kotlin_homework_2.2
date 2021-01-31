ea233b8207adf6799c8c0dd925b525870e1bb74e
Nadia Smirnykh <nadianizh0gmail.com>
2021-01-31 14:31:21 +0200

    main.kt, modified code lines 17 and 18

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,8 +2,8 @@ package ru.netology
 
 fun main() {
     val likes1 = 313
-    val likes2 = 12
-    val likes3 = 1024
+    val likes2 = 102
+    val likes3 = 1025
 
     printout(likes1)
     printout(likes2)
@@ -14,8 +14,9 @@ fun printout(like: Int) {
     val people: String
     val lastDigit: Int
     val last2Digit: Int
-    lastDigit = like - ((like / 10).toInt()) * 10
-    last2Digit = like - ((like / 100).toInt()) * 100
+
+    lastDigit = like%10
+    last2Digit = like%100
 
     if ((lastDigit > 1 && lastDigit < 5) && !(last2Digit > 10 && last2Digit < 20)) {
         people = "человека"
